18fa3a831dc0a44e0a07b9efce420957a727dbc3
msc0001 <mangalsinghchouhan786@gmail.com>
2021-04-18 00:13:04 +0530

    updated sorting and seaching implementation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { ItemI } from "src/models/item.interface";
+
 export interface AppLocationI {
   path: string;
   query: string;
@@ -6,7 +8,7 @@ export interface AppLocationI {
 
 export const getUrlSearchParams = (normalisedParamString): AppLocationI => {
   const [ path, query ] = normalisedParamString.split('?');
-  const params = {};
+  let params = {};
 
   if (query) {
     const keyAndParams: Array<string> = query.split('&');
@@ -40,29 +42,33 @@ enum SortTypes {
 
 export const sortConfig = [
   {
-    key: 'title',
+    key: 'Title',
+    value: 'name',
     type: SortTypes.string,
     // order: 'asc' // can not change as not implemented
   },
   {
-    key: 'dateLastEdited',
+    key: 'Date Last Edited',
+    value: 'dateLastEdited',
     type: SortTypes.date,
     // order: 'desc' //
   }
 ];
 
 export const sortItems = (itemsToSort: Array<any>, sortOption: any) => {
+  if (itemsToSort.length <= 1) return itemsToSort;
   switch(sortOption.type) {
     case SortTypes.string: {
-      return itemsToSort.sort((item1, item2) => item1[sortOption.key] < item2[sortOption.key] ? 1 : -1)
+      return itemsToSort.sort((item1, item2) =>
+        item1[sortOption.value] > item2[sortOption.value] ? 1 : -1
+        );
     }
 
     case SortTypes.date: {
       return itemsToSort.sort((item1, item2) => {
-        const date1 = new Date(item1[sortOption.key]);
-        const date2 = new Date(item2[sortOption.key]);
-
-        return date1>date2 ? -1 : 1
+        const date1 = new Date(item1[sortOption.value]);
+        const date2 = new Date(item2[sortOption.value]);
+        return date1 > date2 ? -1 : 1;
       });
     }
 
@@ -73,6 +79,35 @@ export const sortItems = (itemsToSort: Array<any>, sortOption: any) => {
 }
 
 export const filterByMatchedQuery = (itemsToFilter: Array<any>, query: string) => {
-  console.log('matching query', query)
-  return itemsToFilter.filter((d, i) => i<40);
+  let isExactMatch = false;
+
+  const [pre, mid] = query.split('"');
+  if (mid) {
+    isExactMatch = true;
+    query = mid;
+  }
+
+  return itemsToFilter.filter((itemData: ItemI) => {
+    const { name, description } = itemData;
+
+    const check = (matchString: string) => {
+      const regex = new RegExp(matchString, 'gi')
+      let matchedArray = regex.exec(name);
+      if (matchedArray && matchedArray.length>0) return true;
+
+      matchedArray = regex.exec(description);
+      if (matchedArray && matchedArray.length>0) return true;
+
+      return false;
+    }
+
+    if (isExactMatch) {
+      return check(query);
+    }
+
+    return query
+      .split(" ")
+      .map(matchString => check(matchString))
+      .some(value => value);
+  });
 }
