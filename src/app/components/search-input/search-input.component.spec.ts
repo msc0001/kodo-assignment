589e27d393c2691ea97f2adfc1e3d3d19ad5848b
msc0001 <mangalsinghchouhan786@gmail.com>
2021-04-18 22:02:41 +0530

    added test cases for search input

--- src/app/components/search-input/search-input.component.spec.ts
+++ src/app/components/search-input/search-input.component.spec.ts
@@ -1,4 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Location } from '@angular/common';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { locationMock, MockActivatedRoute } from 'src/mock';
 
 import { SearchInputComponent } from './search-input.component';
 
@@ -8,7 +14,16 @@ describe('SearchInputComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchInputComponent ]
+      declarations: [ SearchInputComponent ],
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+      ],
+      providers: [
+        { provide: Location, useValue: locationMock },
+        { provide: ActivatedRoute, useValue: MockActivatedRoute() }
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +37,70 @@ describe('SearchInputComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set initial value in the input box from route', () => {
+    component.route.snapshot.queryParams.query = 'abc';
+
+    fixture.detectChanges();
+    component.ngOnInit();
+
+    expect(component.value).toBe('abc');
+  })
+
+  it('should call handleOnChange method', () => {
+    const handleOnChangeSpy = spyOn(component, 'handleOnChange').and.callThrough();
+    const value = 'search text';
+    const input = fixture.debugElement.query(By.css('.search-wrapper input'))
+    input.triggerEventHandler('change', { target: { value } });
+    fixture.detectChanges();
+
+    expect(handleOnChangeSpy).toHaveBeenCalled();
+    expect(component.value).toBe(value);
+    expect(component.location.go).toHaveBeenCalledWith('/?query='+value);
+  })
+
+  describe('Will test clear button', () => {
+    it('should not mount clear button', () => {
+      expect(fixture.nativeElement.querySelector('.fa-times')).toBeFalsy()
+    })
+    it('should clear value form search input', () => {
+      component.value = 'searchText';
+      const handleOnChangeSpy = spyOn(component, 'handleOnChange');
+      const clearSearchValueSpy = spyOn(component, 'clearSearchValue').and.callThrough();
+
+      fixture.detectChanges();
+
+      const closeButton = fixture.debugElement.query(By.css('i.fa-times'));
+      closeButton.nativeElement.click();
+
+      expect(clearSearchValueSpy).toHaveBeenCalled();
+      expect(handleOnChangeSpy).toHaveBeenCalledWith({target: { value: '' }})
+    })
+  })
+
+  it('Will open alert box', () => {
+    const handleOnChangeSpy = spyOn(component, 'handleOnChange').and.callThrough();
+    const alertSpy = spyOn(window, 'alert');
+    const value = "i am searching with \\ simpletext with b-slash";
+    const input = fixture.debugElement.query(By.css('.search-wrapper input'))
+    const $event = { target: { value } };
+    input.triggerEventHandler('change', $event);
+
+    expect(handleOnChangeSpy).toHaveBeenCalledWith($event);
+    expect(alertSpy).toHaveBeenCalledWith('Can not use "\\" in search query!!');
+    expect(component.value).toBe('');
+    expect(component.location.go).not.toHaveBeenCalled();
+  })
+
+  it('Will clear value from location search query', () => {
+    const handleOnChangeSpy = spyOn(component, 'handleOnChange').and.callThrough();
+    const value = '';
+    const input = fixture.debugElement.query(By.css('.search-wrapper input'))
+    const $event = { target: { value } };
+    input.triggerEventHandler('change', $event);
+
+    expect(handleOnChangeSpy).toHaveBeenCalledWith($event);
+    expect(component.location.go).toHaveBeenCalledWith('/');
+  })
+
 });
